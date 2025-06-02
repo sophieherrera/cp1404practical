1175e11bd6385ac5c11b74a05f78f2109804eba9
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-02 12:03:54 +1000

    Reuse score function

---
+++ prac_02/score_menu.py
@@ -0,0 +1,12 @@
+"""Program to evaluate a user's score with a menu of options"""
+
+def determine_result(score):
+    """Return result based on the score."""
+    if score < 0 or score > 100:
+        return "Invalid score"
+    elif score >= 90:
+        return "Excellent"
+    elif score >= 50:
+        return "Passable"
+    else:
+        return "Bad"
