16809c73b833ad6bb734ac103cbf00bd756af761
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-02 11:58:12 +1000

    Refactor score program

--- prac_02/broken_score.py
+++ prac_02/broken_score.py
@@ -1,11 +1,17 @@
+def main():
+    score = float(input("Enter score: "))
+    determine_result(score)
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def determine_result(score):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
