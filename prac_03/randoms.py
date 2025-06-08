531c636d9b0cffed58c332d3e35c353941464546
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-08 14:58:58 +1000

    Import random and use examples

---
+++ prac_03/randoms.py
@@ -0,0 +1,25 @@
+# import random
+#
+# print(random.randint(5, 20))  # line 1
+#Answers
+#9
+#17
+#18
+#Smallest number possible would've been 5. Largest would've been 20.
+
+
+# print(random.randrange(3, 10, 2))  # line 2
+#Answers
+#3
+#9
+#3
+#Smallest number could've been 2. Largest possible 10. Could not have produced 4.
+
+# print(random.uniform(2.5, 5.5))  # line 3
+#3.1295076864428557
+#5.157959952578802
+#4.852982443274875
+#Smallest possible is 2.5, largest is 5.5
+
+#Producing a random number between 1 and 100, inclusive.
+print(random.randint(1, 100))
