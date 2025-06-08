c76952cbd3d0c764d5504227057aa244a9e9c699
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-08 16:16:41 +1000

    Write code for files, nr3

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,12 +1,17 @@
-# #1.
-# name = input("Enter your name: ")
-# out_file = open("name.txt", "w")
-# print(name, file=out_file)
-# out_file.close()
+#1.
+name = input("Enter your name: ")
+out_file = open("name.txt", "w")
+print(name, file=out_file)
+out_file.close()
 
-# #2
-# in_file = open("name.txt", "r")
-# name = in_file.read().strip()
-# in_file.close()
-# print(f"Hi {name}!")
+#2
+in_file = open("name.txt", "r")
+name = in_file.read().strip()
+in_file.close()
+print(f"Hi {name}!")
 
+#3
+with open("numbers.txt", "r") as in_file:
+    num1 = int(in_file.readline())
+    num2 = int(in_file.readline())
+    print(num1 + num2)  # Should print 59
