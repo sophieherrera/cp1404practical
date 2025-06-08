1bd28699556c18724834b1a413657f0bfb49ba65
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-08 16:12:41 +1000

    Write code for files, nr2

--- prac_03/files.py
+++ prac_03/files.py
@@ -1,5 +1,12 @@
-#1.
-name = input("Enter your name: ")
-out_file = open("name.txt", "w")
-print(name, file=out_file)
-out_file.close()
+# #1.
+# name = input("Enter your name: ")
+# out_file = open("name.txt", "w")
+# print(name, file=out_file)
+# out_file.close()
+
+# #2
+# in_file = open("name.txt", "r")
+# name = in_file.read().strip()
+# in_file.close()
+# print(f"Hi {name}!")
+
