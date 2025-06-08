f41cdec08439d496ff7f72f7704d2ff576502034
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-08 15:32:48 +1000

    Complete capitalist_conrad.py with file output

--- prac_03/capitalist_conrad.py
+++ prac_03/capitalist_conrad.py
@@ -16,22 +16,22 @@ MAX_PRICE = 100.0
 INITIAL_PRICE = 10.0
 OUTPUT_FILE = "stock_prices.txt"
 
+price=INITIAL_PRICE
+number_of_days= 0
+out_file = open(OUTPUT_FILE, "w")
+print(f"Starting price: ${price:,.2f}",file=out_file)
 
-price = INITIAL_PRICE
-print(f"${price:,.2f}")
+
+
 
 while MIN_PRICE <= price <= MAX_PRICE:
-    price_change = 0
-    # generate a random integer of 1 or 2
-    # if it's 1, the price increases, otherwise it decreases
+    number_of_days += 1
     if random.randint(1, 2) == 1:
-        # generate a random floating-point number
-        # between 0 and MAX_INCREASE
         price_change = random.uniform(0, MAX_INCREASE)
     else:
-        # generate a random floating-point number
-        # between negative MAX_DECREASE and 0
-        price_change = random.uniform(-MAX_DECREASE, 0)
+        price_change = -random.uniform(0, MAX_DECREASE)
 
     price *= (1 + price_change)
-    print(f"${price:,.2f}")
+    print(f"On day {number_of_days} price is: ${price:,.2f}", file=out_file)
+
+out_file.close()
