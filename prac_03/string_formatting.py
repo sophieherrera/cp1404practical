43d4f7c99e9387b805b4f6f299165dbb255f0cc7
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-08 14:45:02 +1000

    Use f-string formatting to produce the output

--- prac_03/string_formatting.py
+++ prac_03/string_formatting.py
@@ -32,27 +32,12 @@ numbers = [1, 19, 123, 456, -25]
 for i, number in enumerate(numbers, 1):
     print(f"Number {i} is {number:5}")
 
-# TODO: Use f-string formatting to produce the output:
-# 1922 Gibson L-5 CES for about $16,036!
-
-# TODO: Using a for loop with the range function and f-string formatting,
-# produce the following right-aligned output (DO NOT use a list):
-# 2 ^ 0 is    1
-# 2 ^ 1 is    2
-# 2 ^ 2 is    4
-# 2 ^ 3 is    8
-# 2 ^ 4 is   16
-# 2 ^ 5 is   32
-# 2 ^ 6 is   64
-# 2 ^ 7 is  128
-# 2 ^ 8 is  256
-# 2 ^ 9 is  512
-# 2 ^10 is 1024
-
 
 # Creating an F-string
 name = "Gibson L-5 CES"
 year = 1922
 cost = 16035.99
 
-print(f"{year} {name} for about ${cost:,.0f}!")
+# Creating a Loop with f-strings, powers of 2
+for i in range(0, 11):
+    print(f"2 ^ {i:2} is {2 ** i:4}")
