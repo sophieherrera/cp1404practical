7c69cda4c454ea0e1bd309b4063ac3e58cde9b8e
sophieherrera <sophieisabel.herrera@my.jcu.edu.au>
2025-06-22 13:34:15 +1000

    Make input case-insensitive in state_name.py

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,7 +4,7 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
+# Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {
     "QLD": "Queensland",
     "NSW": "New South Wales",
@@ -18,10 +18,11 @@ CODE_TO_NAME = {
 
 print(CODE_TO_NAME)
 
-state_code = input("Enter short state: ")
+state_code = input("Enter short state: ").upper()
 while state_code != "":
     if state_code in CODE_TO_NAME:
         print(state_code, "is", CODE_TO_NAME[state_code])
     else:
         print("Invalid short state")
-    state_code = input("Enter short state: ")
+    state_code = input("Enter short state: ").upper()
+
